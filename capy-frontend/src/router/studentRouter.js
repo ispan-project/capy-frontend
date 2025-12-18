9243d40425849198758b50f7545eeb483246493e
IanHsu <kiwihsu828@gmail.com>
2025-12-18 10:54:44 +0800

    修改願望+進度調

--- capy-frontend/src/router/studentRouter.js
+++ capy-frontend/src/router/studentRouter.js
@@ -28,14 +28,6 @@ const studentRoutes = [
         path: "explore",
         name: "courseExplore",
         component: () => import("@/views/student/ExplorePage/CourseExplorePage.vue"),
-        beforeEnter: async (to, from, next) => {
-          try {
-            await searchCourses();
-          } catch (error) {
-            console.error("預先載入探索課程失敗，將繼續導向頁面：", error);
-          }
-          next();
-        },
       },
       // 課程詳情頁面
       {
